209fe0aaf82ff6e12fa5ab66059c850075b6a39b
WattWozy <niklas2@gmx.es>
2025-05-26 22:04:49 +0200

    trimming the separation for published/pending

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ export default function BlogPage() {
     {
       title: "Building a second brain: a new purpose.",
       content: `Having almost finished my web-portfolio, I bumped into this book.
-        ---------------------------------------------  
+        ------------------------------------------ 
         PENDING. ⏳
       `,
       link: "/blog",
@@ -16,7 +16,7 @@ export default function BlogPage() {
     {
       title: "GIT: the information manager from hell",
       content: `GIT, its history, and some basic concepts.
-      ---------------------------------------------
+      ------------------------------------------
       PENDING. ⏳
       `,
       link: "/blog",
@@ -25,7 +25,7 @@ export default function BlogPage() {
       title:
         "Teaching a computer to learn: A summary of Nielsen's book about Machine Learning and Deep Neural Networks.",
       content: `Summary of this great read.
-      ---------------------------------------------
+      ------------------------------------------
       PENDING. ⏳
       `,
       link: "/blog",
@@ -33,7 +33,7 @@ export default function BlogPage() {
     {
       title: "Code Obfuscation: meaning, techniques and tools",
       content: `Obfuscation's goals and mechanisms. A brief introduction.
-      ---------------------------------------------
+      ------------------------------------------
       PUBLISHED. ✅
       `,
       link: "/blog/fourthEntry",
@@ -41,7 +41,7 @@ export default function BlogPage() {
     {
       title: "A brief introduction to DEVOPS.",
       content: `From commit and pushing to the right branches, through code review and deployment.
-        ---------------------------------------------
+        ------------------------------------------
         PUBLISED. ✅
         `,
       link: "/blog/thirdEntry",
@@ -50,14 +50,14 @@ export default function BlogPage() {
       title:
         "Building a Langchain4j chatbot agent for banking customer service.",
       content: `Project done internally together with 15 other co-workers. I describe the general architecture of the service, the technologies, and the teamwork experience.
-        ---------------------------------------------
+        ------------------------------------------
         PUBLISED. ✅`,
       link: "/blog/secondEntry",
     },
     {
       title: "Working experience first year: A brief summary.",
       content: `In this blog post, I cover the first impressions of starting an amazing journey at a huge fintech company.
-        ---------------------------------------------
+        ------------------------------------------
         PUBLISED. ✅
         `,
       link: "/blog/firstEntry",
